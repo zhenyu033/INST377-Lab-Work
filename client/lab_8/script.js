ae2730b56e9c5de3dafa0f49c3eb431437564a4b
zhenyu033 <zyu1@terpmail.umd.edu>
2023-04-17 00:48:41 -0400

    update

--- client/lab_8/script.js
+++ client/lab_8/script.js
@@ -33,6 +33,36 @@ function getRandomIntInclusive(min,max) {
     return lowerCaseName.includes(lowerCaseQuery);
     })
   }
+
+  function initMap() {
+    const carto = L.map('map').setView([38.98, -76.93], 13);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(carto);
+    return carto;
+  
+  }
+  
+  function markerPlace(array, map) {
+  
+    console.log('array for markers', array)
+  
+    map.eachLayer((layer) => {
+      if (layer instanceof L.Marker) {
+        layer.remove();
+      }
+    });
+    
+  
+    array.forEach((item) => {
+      console.log('markerPlace', item);
+      const { coordinates } = item.geocoded_column_1
+      L.marker([coordinates[1], coordinates[0]]).addTo(map);
+  
+    })
+  
+  }
   
   async function mainEvent() {
     const form = document.querySelector('.main_form'); // get your main form so you can do JS with it
@@ -44,6 +74,8 @@ function getRandomIntInclusive(min,max) {
     const loadAnimation = document.querySelector("#data_load_animation");
     loadAnimation.style.display ="none";
     generateListButton.classList.add("hidden");
+
+    const carto = initMap();
 
     let storedList = []; 
     let currentList = [];
